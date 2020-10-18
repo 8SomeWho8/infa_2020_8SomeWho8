881f76d713bf2f102146dd996d4c6e424b8fff55
8SomeWho8 <rozhnik22@gmail.com>
2020-10-18 15:31:28 +0300

    Added a prototype of a new target - ring It appears at random coordinates ant moves along a trajectory shaped like lissajous curve.

--- lab5/task.py
+++ lab5/task.py
@@ -1,4 +1,5 @@
 import pygame
+import math as m
 from pygame.draw import *
 from random import randint
 
@@ -18,7 +19,10 @@ BLACK = (0, 0, 0)
 LILAC = (182, 102, 210)
 AMBER = (255, 191, 0)
 VINOUS = (113, 25, 25)
-COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN, LILAC, AMBER, VINOUS]
+RED_CORAL = (255, 127, 80)
+BLUE_STEEL = (80, 127, 255)
+LIME = (204, 255, 0)
+COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN, LILAC, AMBER, VINOUS, RED_CORAL, BLUE_STEEL, LIME]
 
 
 def ballz(dt: float, n: int, x: list, y: list, velocity_x: list, velocity_y: list, r: list, color: list, hit: list):
@@ -46,7 +50,7 @@ def ballz(dt: float, n: int, x: list, y: list, velocity_x: list, velocity_y: lis
             y[i] = (randint(r[i], 900 - r[i]))
             velocity_x[i] = (randint(-100, 100))
             velocity_y[i] = (randint(-100, 100))
-            color[i] = (COLORS[randint(0, 8)])
+            color[i] = (COLORS[randint(0, 11)])
             hit[i] = False
         # Drawing a circle at current state
         circle(screen, color[i], (round(x[i]), round(y[i])), r[i])
@@ -55,53 +59,84 @@ def ballz(dt: float, n: int, x: list, y: list, velocity_x: list, velocity_y: lis
         y[i] += velocity_y[i] * dt
 
 
+def ringz(t: float, x_2: list, y_2: list, n_2: int, r_2: list, x_0: list, y_0: list, a_x: list, a_y: list, color: list):
+    for i in range(n_2):
+        circle(screen, color[i], (round(x_2[i]), round(y_2[i])), r_2[i])
+        circle(screen, BLACK, (round(x_2[i]), round(y_2[i])), r_2[i]-5)
+        x_2[i] = x_0[i] + a_x[i] * m.cos(0.7 * t)
+        y_2[i] = y_0[i] + a_y[i] * m.sin(1.4 * t)
+
+
 clock = pygame.time.Clock()
 finished = False
-
-n = 10  # Number of circles created
-r = []  # List of radii of circles
-x = []  # List of current horizontal coordinates of circles
-y = []  # List of current vertical coordinates of circles
-velocity_x = []  # List of current horizontal velocities of circles
-velocity_y = []  # List of current vertical velocities of circles
-color = []  # List of colours of circles
-hit = []  # List of bool variables for each circle that is True if user has clicked on the circle
-# and False if circle hasn't been clicked.
+n_1 = 5  # Number of circles created
+r_1 = []  # List of radii of circles
+x_1 = []  # List of current horizontal coordinates of circles
+y_1 = []  # List of current vertical coordinates of circles
+velocity_x_1 = []  # List of current horizontal velocities of circles
+velocity_y_1 = []  # List of current vertical velocities of circles
+color_1 = []  # List of colours of circles
+hit_1 = []  # List of bool variables for each circle that is True if user has clicked on the circle
+# and False if circle hasn't been clicked on.
 
 # Creating lists of variables of circles with random values
-for i in range(n):
-    r.append(randint(10, 100))
-    x.append(randint(r[i], 1200 - r[i]))
-    y.append(randint(r[i], 900 - r[i]))
-    velocity_x.append(randint(-100, 100))
-    velocity_y.append(randint(-100, 100))
-    color.append(COLORS[randint(0, 8)])
-    hit.append(False)
+for i in range(n_1):
+    r_1.append(randint(10, 100))
+    x_1.append(randint(r_1[i], 1200 - r_1[i]))
+    y_1.append(randint(r_1[i], 900 - r_1[i]))
+    velocity_x_1.append(randint(-100, 100))
+    velocity_y_1.append(randint(-100, 100))
+    color_1.append(COLORS[randint(0, 11)])
+    hit_1.append(False)
+
+n_2 = 3  # Number of rings created
+r_2 = []  # List of radii of rings
+x_2 = []  # List of current horizontal coordinates of rings
+y_2 = []  # List of current vertical coordinates of rings
+a_x = []  # List of horizontal amplitudes of rings
+a_y = []  # List of vertical amplitudes of rings
+x_0 = []  # List of starting horizontal coordinates of rings
+y_0 = []  # List of starting vertical coordinates of rings
+color_2 = []  # List of colours of rings
+hit_2 = []  # List of bool variables for each ring that is True if user has clicked on the ring
+# and False if ring hasn't been clicked on.
+for i in range(n_2):
+    a_x.append(randint(50, 200))
+    a_y.append(randint(50, 200))
+    r_2.append(randint(10, 20))
+    x_0.append(randint(a_x[i] + r_2[i], 1200 - a_x[i] - r_2[i]))
+    y_0.append(randint(a_y[i] + r_2[i], 900 - a_y[i] - r_2[i]))
+    x_2.append(x_0[i])
+    y_2.append(y_0[i])
+    color_2.append(COLORS[randint(0, 11)])
 
 # Score of the game
 score = 0
 # Step of time in drawing circles
 dt = 0.1
+t = 0
 while not finished:
     clock.tick(FPS)
     screen.fill(BLACK)  # Filling the screen with black to redraw all picture
-    ballz(dt, n, x, y, velocity_x, velocity_y, r, color, hit)  # Drawing circles on current positions
+    ballz(dt, n_1, x_1, y_1, velocity_x_1, velocity_y_1, r_1, color_1, hit_1)  # Drawing circles on current positions
+    ringz(t, x_2, y_2, n_2, r_2, x_0, y_0, a_x, a_y, color_2)
+    t += dt
     # Checking the position of circles relative to edges in order to change sign of horizontal or vertical velocity
     # if circle is going out of them.
-    for i in range(n):
-        if x[i] + r[i] >= 1200 or x[i] - r[i] <= 0:
-            velocity_x[i] *= -1
-        if y[i] - r[i] <= 0 or y[i] + r[i] >= 900:
-            velocity_y[i] *= -1
+    for i in range(n_1):
+        if x_1[i] + r_1[i] >= 1200 or x_1[i] - r_1[i] <= 0:
+            velocity_x_1[i] *= -1
+        if y_1[i] - r_1[i] <= 0 or y_1[i] + r_1[i] >= 900:
+            velocity_y_1[i] *= -1
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
         # Checking if user has clicked on a circle and changing the value of hit[] of clicked circle
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            for i in range(n):
-                if (event.pos[0] - x[i]) ** 2 + (event.pos[1] - y[i]) ** 2 <= r[i] ** 2:
-                    hit[i] = True
+            for i in range(n_1):
+                if (event.pos[0] - x_1[i]) ** 2 + (event.pos[1] - y_1[i]) ** 2 <= r_1[i] ** 2:
+                    hit_1[i] = True
                     score += 1  # Adding one point for one clicked circle
 print(score)
 pygame.quit()
